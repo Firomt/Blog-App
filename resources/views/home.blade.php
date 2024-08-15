d241083bd1aef2125f1e75d86fee67e95e1f1190
Firomt <firomsamt@gmail.com>
2024-08-15 14:30:26 +0300

    add navabar and banner image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,26 @@
 
     <title>Blog App</title>
 </head>
-<body>
+<body class="bg-light">
+    <div class="row g-0">
+        <div class="col-12 p-2 ps-4 pe-4 m-0">
+            <nav class="navbar navbar-light mt-2 mb-2 p-0">
+                <div class="container-fluid p-0">
+                    <a class="navbar-brand" href="/"><b>Blog</b></a>
+                    <a class="" href="/login"><b>Log In</b></a> 
+                </div>
+            </nav>
+
+        </div>
+        <div class="col-12  g-0" style="background: white; border-bottom: 1px solid black; border-top: 1px solid black"> 
+            <img style="max-height:420px;" src="/laravel-blog-background-image.png" title="My Blog Main Image">
+
+
+        </div>
+    </div>
+
+
+
 
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
