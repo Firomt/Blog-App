da13876509d30fc74d983e6c6c717299bcbdd008
Firomt <firomsamt@gmail.com>
2024-08-16 16:03:32 +0300

    add trending and recently written blogs section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,15 +47,37 @@
                 @endforeach
 
             </div>
-            <div class="col-lg-1 col-0">
-
+            <div class="col-lg-1 col-0"></div>
+            <div class="col-lg-3 col-12 mt-5 ps-lg-4" >
+                <div class="row">
+                    <div class="col-12 pb-4 g-0 border-bottom-black">
+                         <p class="text-muted fs-5">What is trending </p>
+                         @foreach ($trending as $trend )
+                            <div class="card p-0 mt-4 small-card">
+                                <img src="{{ $trend['image_url_landscape'] }}" class="w-100 h-100 card-img" style="opacity: 0.5">
+                                <div class="card-img-overlay">
+                                    <h5 class="card-title">{{ $trend['title'] }}</h5>
+                                    <p class="card-text">{{ $trend['date'] }}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="col-12 pt-3 pb-4 g-0 border-bottom-black">
+                        <p class="text-muted fs-5">Recently Written</p>
+                        @foreach ($recentlyWritten as $recent )
+                           <div class="card p-0 mt-4 small-card">
+                               <img src="{{ $recent['image_url_landscape'] }}" class="w-100 h-100 card-img" style="opacity: 0.5">
+                               <div class="card-img-overlay">
+                                   <h5 class="card-title">{{ $recent['title'] }}</h5>
+                                   <p class="card-text">{{ $recent['date'] }}</p>
+                               </div>
+                           </div>
+                       @endforeach
+                   </div>
+                   
+                </div>
             </div>
-            <div class="col-lg-3 col-12" >
-
-            </div>
-
         </div>
-
     </div>
 
 
--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,110 @@ Route::get('/', function () {
 
 
             ]
+            ],
+
+            "trending"=>[
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ],
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ],
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ]
+            ],
+
+            "recentlyWritten"=>[
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ],
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ],
+            [
+                "url"=> "",
+                "is_trending"=> true,
+                "author_image_url"=> "https://avatars.githubusercontent.com/u/147161286?v=4",
+                "author"=>"Firomsa Mitiku",
+                "image_url_portrait" => "https://picsum.photos/300/350",
+                "image_url_landscape"=>"https://picsum.photos/360/160",
+                "title"=>"Laravel Websockets | Chat Application Example",
+                "date"=>"July 27, 2024",
+                "description"=>"This course is about how to implements custom laravel web socket chat application",
+                "tags"=>"tag1, tag2,tag3"
+
+
+
+
+            ]
         ]
+
+
     ]);
 });
